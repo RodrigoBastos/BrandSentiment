acbdfa6582c436807dd8b20650c50969776baaa5
Rodrigo Bastos <rodrigorozendobastos@gmail.com>
2015-11-17 08:53:00 -0300

    Added expression-session

--- twitter-io.js
+++ twitter-io.js
@@ -2,45 +2,71 @@
  * Created by rodrigo on 15/11/15.
  */
 
-var express     = require('express');
-var bodyParser  = require('body-parser');
-var twitterClient     = require('./server/config/index.js').twitterClient;
-var analysisSentiment = require('./server/sentiment/sentiment.js');
-var path        = require('path');
+var path                  = require('path');
+var express               = require('express');
+var bodyParser            = require('body-parser');
+var cookieParser          = require('cookie-parser');
+var expressSession        = require('express-session');
+
+var twitterClient         = require('./server/config/index.js').twitterClient;
+var analysisSentiment     = require('./server/sentiment/sentiment.js');
+
+var cookie = cookieParser(process.env.SESSION_SECRET);
+var store  = new expressSession.MemoryStore();
+
 var app = express();
+
 
 app.set('views', path.join(__dirname, 'client', 'views'));
 app.set('view engine', 'jade');
 
 app.use(bodyParser.json());
+app.use(expressSession({
+  secret: process.env.SESSION_SECRET,
+  name: process.env.SESSION_KEY,
+  resave: true,
+  saveUninitialized: true,
+  store: store
+}));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'client', 'public')));
 
-app.get('/', function (req, res) {
 
+var stream = null;
+
+app.get('/', function (req, res) {
   res.render('index', {title: 'BRAND SENTIMENT'});
 });
 
 app.post('/search', function (req, res) {
 
+  console.log(req.session);
   var query = req.body.query;
   io.sockets.on('connection', function (socket) {
+
+    var session = socket.handshake.session;
+    if(session.stream != null)
+      session.stream.stop();
+
+    session.pos = 0;
+    session.neg = 0;
+    console.log('session', session);
     console.log('Connected');
     var countNegative = 0;
     var countPositive = 0;
-    var stream = twitterClient.stream('statuses/filter', {
+    session.stream = twitterClient.stream('statuses/filter', {
       track: query
     });
 
-    stream.on('tweet', function (tweet) {
+    session.stream.on('tweet', function (tweet) {
 
       console.log('tweet', tweet.text);
       var result = analysisSentiment.classify(tweet.text);
       if (result == 'positive')
-        countPositive++;
+        session.pos++;
       else
-        countNegative++;
-      io.sockets.emit('stream', {pos:countPositive, neg:countNegative});
+        session.neg++;
+      io.sockets.emit('stream', {pos:session.pos, neg:session.neg});
 
     });
   });
@@ -52,7 +78,20 @@ app.post('/search', function (req, res) {
 var io = require("socket.io").listen(app.listen(3800));
 
 
-
+io.use(function(socket, next) {
+  var data = socket.request;
+  cookie(data, {}, function(err) {
+    var sessionID = data.signedCookies[process.env.SESSION_KEY];
+    store.get(sessionID, function(err, session) {
+      if (err || !session) {
+        return next(new Error('Acesso negado!'));
+      } else {
+        socket.handshake.session = session;
+        return next();
+      }
+    });
+  });
+});
 //io.sockets.on('connection', function (socket) {
 //  var stream = twitterClient.stream('statuses/filter', {
 //    'track':'playstation',
