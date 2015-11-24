a61516bb8e4f78be2d3a5cd3089664d1363d7426
Rodrigo Bastos <rodrigorozendobastos@gmail.com>
2015-11-24 04:24:03 -0300

    Created Dashboard for Sentiment Analysis

--- client/source/js/partials/bs-controllers.js
+++ client/source/js/partials/bs-controllers.js
@@ -8,12 +8,31 @@ angular.module('application.controllers', ['chart.js', 'socket-io'])
   .controller('DashboardCtrl', function ($scope, socket) {
     $scope.name='Rodrigo';
 
-    $scope.labels = ["Positivo", "Negativo"];
-    $scope.data = [0, 0];
+    $scope.labels = ["Positivo", "Neutra", "Negativo"];
+    $scope.data = [1, 1, 1];
+
+
+    $scope.score = {
+      posSentence: 'Frase Positiva de Maior Relevância!',
+      negSentence: 'Frase Negativa de Maior Relevância!'
+    };
+
+
+    $scope.getEmotion = function () {
+      if ($scope.data[0] > $scope.data[1] && $scope.data[0] > $scope.data[2])
+        return 'images/happy.png';
+      else if ($scope.data[1] > $scope.data[0] && $scope.data[1] > $scope.data[2])
+        return 'images/angry.png';
+      else
+        return 'images/normal.png';
+    };
 
     socket.on('stream', function (score) {
       console.log(score);
-      $scope.data = [score.pos, score.neg]
+      $scope.data = [score.pos, score.ntr, score.neg];
+      $scope.score.posSentence = score.posSentence;
+      $scope.score.negSentence = score.negSentence;
+
     });
 
   });
