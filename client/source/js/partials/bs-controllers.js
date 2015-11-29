58462b9d5b49af972ec7659630263cbbf574dd3c
Rodrigo Bastos <rodrigorozendobastos@gmail.com>
2015-11-29 19:04:24 -0300

    New Dashboard Layout

--- client/source/js/partials/bs-controllers.js
+++ client/source/js/partials/bs-controllers.js
@@ -14,14 +14,15 @@ angular.module('application.controllers', ['chart.js', 'socket-io'])
 
     $scope.score = {
       posSentence: 'Frase Positiva de Maior Relevância!',
-      negSentence: 'Frase Negativa de Maior Relevância!'
+      negSentence: 'Frase Negativa de Maior Relevância!',
+      total: 0
     };
 
 
     $scope.getEmotion = function () {
       if ($scope.data[0] > $scope.data[1] && $scope.data[0] > $scope.data[2])
         return 'images/happy.png';
-      else if ($scope.data[1] > $scope.data[0] && $scope.data[1] > $scope.data[2])
+      else if ($scope.data[2] > $scope.data[0] && $scope.data[2] > $scope.data[1])
         return 'images/angry.png';
       else
         return 'images/normal.png';
@@ -29,9 +30,10 @@ angular.module('application.controllers', ['chart.js', 'socket-io'])
 
     socket.on('stream', function (score) {
       console.log(score);
+      $scope.score.total = score.pos + score.ntr + score.neg;
       $scope.data = [score.pos, score.ntr, score.neg];
-      $scope.score.posSentence = score.posSentence;
-      $scope.score.negSentence = score.negSentence;
+      $scope.score.posSentence = score.posSentence != '' ? score.posSentence: $scope.score.posSentence;
+      $scope.score.negSentence = score.negSentence != '' ? score.negSentence: $scope.score.negSentence;
 
     });
 
