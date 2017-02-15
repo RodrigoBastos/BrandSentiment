9f229546a8029a16ed8ef7ffa3fd92019f267990
rodrigorozendobastos@gmail.com <rodrigorozendobastos@gmail.com>
2017-02-15 00:02:22 -0300

    Organiza script para limpeza de frases

--- server/script/clean-sentences.js
+++ server/script/clean-sentences.js
@@ -1,26 +1,22 @@
-var fs    = require("fs");
-var _     = require("lodash");
+var fs = require('fs');
+var _ = require('lodash');
 
-var negatives = fs.readFileSync("sentences.txt", "utf8").split("\n");
-
-var cleanNegatives = [];
-var array = [];
-
-negatives.map(function(sentence){
-  console.log("Sentence: ", sentence);
-  array = sentence.split(" ");
-  console.log("Array: ", array);
-
-  array = removeLinks(array);
-  console.log("Sem Links: ", array);
-});
-
-
-function removeLinks (array) {
-    var arrayWithoutLinks = array.map(function (item) {
-      if(!_.include(item, "http")){
-        return item.trim();
-      }
-    });
-    return arrayWithoutLinks;
+// Remove links das frases
+function removeLinks(words) {
+  return words.map(function (word) {
+    if (!_.include(word, 'http')) return word.trim();
+    return false;
+  }).filter(Boolean);
 }
+
+// Lendo frases negativas
+var negatives = fs.readFileSync('sentences.txt', 'utf8').split('\n');
+
+// Percorre o array de frases negativas
+negatives.forEach(function (sentence) {
+  console.log('Sentence: ', sentence);
+  var words = sentence.split(' ');
+  console.log('Array: ', words);
+  words = removeLinks(words);
+  console.log('Sem Links: ', words);
+});
