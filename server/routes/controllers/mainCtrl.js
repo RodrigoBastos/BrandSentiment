7cd1b71fd4278c39ecf9ec7e235f3b504205555f
rodrigorozendobastos@gmail.com <rodrigorozendobastos@gmail.com>
2017-02-15 00:01:01 -0300

    Organiza rotas de acordo com eslint

--- server/routes/controllers/mainCtrl.js
+++ server/routes/controllers/mainCtrl.js
@@ -1,36 +1,19 @@
-var twitterClient = require("../../config/index.js").twitterClient;
+var twitterClient = require('../../config/index.js').twitterClient;
 
-exports.getIndex = function (req, res) {
-  res.render("index");
-};
+exports.getIndex = function (req, res) { res.render('index'); };
 
 exports.getWatchTwitter = function (req, res) {
   var stream;
   var testTweetCount = 0;
-  var phrase = "bieber";
-  // var phrase = "ice cream";
+  var phrase = 'bieber';
 
-  stream = twitterClient.stream("statuses/filter", { track: phrase });
-  res.send("Monitoring Twitter for \"" + phrase + "\"...  Logging Twitter traffic.");
-  stream.on("tweet", function (tweet) {
-    testTweetCount++;
-    if(testTweetCount == 10){
+  stream = twitterClient.stream('statuses/filter', { track: phrase });
+  res.send('Monitoring Twitter for \'' + phrase + '\'...  Logging Twitter traffic.');
+  stream.on('tweet', function (tweet) {
+    testTweetCount += 1;
+    if (testTweetCount === 10) {
       res.send(tweet.text);
-      console.log("Tweet #" + testTweetCount + ":  " + tweet.text);
+      console.log('Tweet #' + testTweetCount + ':  ' + tweet.text);
     }
   });
-
-    //stream = twitterClient.stream("statuses/filter", {
-    //  "track": phrase
-    //}, function (stream) {
-    //  res.send("Monitoring Twitter for \"" + phrase + "\"...  Logging Twitter traffic.");
-    //  stream.on("data", function (data) {
-    //    testTweetCount++;
-    //    // Update the console every 50 analyzed tweets
-    //    if (testTweetCount % 10 === 0) {
-    //      console.log("Tweet #" + testTweetCount + ":  " + data.text);
-    //    }
-    //  });
-    //});
-
 };
--- server/routes/mainRoutes.js
+++ server/routes/mainRoutes.js
@@ -1,7 +1,7 @@
-var express = require("express");
-var mainCtrl = require("./controllers/mainCtrl");
+var express = require('express');
+var mainCtrl = require('./controllers/mainCtrl');
 
 // Rotas
 module.exports = new express.Router()
-  .get("/", mainCtrl.getIndex)
-  .get("/watch", mainCtrl.getWatchTwitter);
+  .get('/', mainCtrl.getIndex)
+  .get('/watch', mainCtrl.getWatchTwitter);
