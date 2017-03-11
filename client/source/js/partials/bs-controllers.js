d64613da5418b4c8cd16f8d113cafbd1a0aa42e3
rodrigorozendobastos@gmail.com <rodrigorozendobastos@gmail.com>
2017-03-11 00:44:38 -0300

    Organiza código de acordo com o eslint

--- client/source/js/partials/bs-controllers.js
+++ client/source/js/partials/bs-controllers.js
@@ -1,35 +1,27 @@
-angular.module("application.controllers", ["chart.js", "socket-io"])
+angular.module('application.controllers', ['chart.js', 'socket-io'])
+  .controller('DashboardCtrl', function ($scope, socket) {
+    $scope.name = 'Rodrigo';
 
-  .controller("DashboardCtrl", function ($scope, socket) {
-    $scope.name="Rodrigo";
-
-    $scope.labels = ["Positivo", "Neutra", "Negativo"];
+    $scope.labels = ['Positivo', 'Neutra', 'Negativo'];
     $scope.data = [1, 1, 1];
-
-
     $scope.score = {
-      posSentence: "Frase Positiva de Maior Relevância!",
-      negSentence: "Frase Negativa de Maior Relevância!",
+      posSentence: 'Frase Positiva de Maior Relevância!',
+      negSentence: 'Frase Negativa de Maior Relevância!',
       total: 0
     };
 
 
     $scope.getEmotion = function () {
-      if ($scope.data[0] > $scope.data[1] && $scope.data[0] > $scope.data[2])
-        return "images/happy.png";
-      else if ($scope.data[2] > $scope.data[0] && $scope.data[2] > $scope.data[1])
-        return "images/angry.png";
-      else
-        return "images/normal.png";
+      if ($scope.data[0] > $scope.data[1] && $scope.data[0] > $scope.data[2]) return 'images/happy.png';
+      else if ($scope.data[2] > $scope.data[0] && $scope.data[2] > $scope.data[1]) return 'images/angry.png';
+      return 'images/normal.png';
     };
 
-    socket.on("stream", function (score) {
+    socket.on('stream', function (score) {
       console.log(score);
       $scope.score.total = score.pos + score.ntr + score.neg;
       $scope.data = [score.pos, score.ntr, score.neg];
-      $scope.score.posSentence = score.posSentence != "" ? score.posSentence: $scope.score.posSentence;
-      $scope.score.negSentence = score.negSentence != "" ? score.negSentence: $scope.score.negSentence;
-
+      $scope.score.posSentence = score.posSentence !== '' ? score.posSentence: $scope.score.posSentence;
+      $scope.score.negSentence = score.negSentence !== '' ? score.negSentence: $scope.score.negSentence;
     });
-
   });
